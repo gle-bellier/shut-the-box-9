87a9aa6658e63bea40ccf0c3d4abb3f62ed26233
georges lebellier <georges.lebeller@gmail.com>
2022-07-30 16:45:15 +0200

    Add training configs

---
+++ src/learn/configs/value_net_config.py
@@ -0,0 +1,15 @@
+import ml_collections
+
+
+def get_config():
+    """Get the default hyperparameter configuration."""
+
+    config = ml_collections.ConfigDict()
+    config.batch_size = 128
+    config.path_dataset = "data/games/games_resumes.pickle"
+
+    config.learning_rate = 1e-2
+    config.momentum = 0.9
+
+    config.num_epochs = 20
+    return config
